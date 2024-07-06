59536f5766141e6aa036a91fe912a1982664c3e2
K SAI PRANEETH REDDY <127538019+PraneethReddy15@users.noreply.github.com>
2024-07-06 20:16:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-var res=document.querySelector(".grid-input");
-var btn=document.querySelectorAll(".button");
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
 
 let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'AC'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+
+        else if(e.target.innerHTML == 'AC'){
+            string = "";
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL'){
+            string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else{
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+        
+    })
 })
 
+// ------------------------THEME------------------
+var icon =document.getElementById("icon");
+icon.onclick=function()
+{
+    document.body.classList.toggle("light-theme");
+    if(document.body.classList.contains("light-theme"))
+    {
+        icon.src="./files/sun.png"
+    }
+    else{
+        icon.src="./files/moon.png"
+
+    }
+}
